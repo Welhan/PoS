4461b4070bb46e09e9a04de71f16618ab4630b07
Welhan <welhan.susanto@gmail.com>
2023-01-25 15:54:38 +0700

    generate submenu sesuai db

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -3,15 +3,27 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\SubmenuModel;
 
 class Dashboard extends BaseController
 {
+
+    protected $submenuModel;
+
+    public function __construct()
+    {
+        $this->submenuModel = new SubmenuModel();
+    }
+
     public function index()
     {
         if (!check_login(session('userID'))) return redirect()->to('/login');
 
+        // dd(generate_menu(session('userID')));
+
         $data = [
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'active' => $this->submenuModel->find(1)
         ];
 
         return view('dashboard/index', $data);
